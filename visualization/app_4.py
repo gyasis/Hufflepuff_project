5203c414a1cbb22880cec4807e8fd0205ab9b5d4
Tobias-Schulz-AI <73252716+Tobias-Schulz-AI@users.noreply.github.com>
2021-01-16 11:55:10 +0100

    Updated Visualization and Preprocessing

--- visualization/app_4.py
+++ visualization/app_4.py
@@ -21,196 +21,209 @@ import plotly.graph_objects as go
 
 
 # just for production ... should be done in seperate Notebook
-
-def preprocessing():
-    df = pd.read_csv("EpicFantasy_2.csv")
-    df.rename(columns={'name':'Title', 'OriginalPDate':'Publishdate', 'author':'Author'}, inplace=True)
-    
-    # cleaning the Title
-    df['Title'] = df['Title'].str.strip()
-    
-    # Calculating number of awards
-    df['Awards'] = df['Awards'].str.strip('[]')
-    df['Awards'] = df['Awards'].replace(r'^\s*$', np.NaN, regex=True)
-    df['Awards_x'] = df.Awards.str.split(',')
-    df['award_count'] = df['Awards_x'].str.len()
-    df['award_count'] = df['award_count'].fillna(0).astype(int)
-    df = df.drop(['Awards_x'], axis= 1)
-    
-    # Cleaning author column from paranthesis text
-    df['Author'] = df.Author.str.replace(r"\(.*Goodreads Author\)","")
-
-    # cleaning number of ratings
-    df.rename(columns={'number of ratings': 'number_of_ratings'}, inplace=True)
-    df['number_of_ratings'] = df['number_of_ratings'].str.replace('[,]',"").str.replace('[^0-9]',"0").fillna(0).astype(int)
-
-    # cleaning avg_rat
-    #df['avg_ratings'] = df.avg_ratings.str.replace('[^0-9.]',"")
-    df['avg_ratings'] = df['avg_ratings'].replace(r'^\s*$', np.NaN, regex=True).astype(float)
-    
-    # cleaning Genre
-    df['Genre'] = df['Genre'].str.strip('[]')
-    
-    # cleaning pages
-    df['Num_pages'] = df['Num_pages'].astype(str)
-    df['Num_pages'] = df.Num_pages.str.replace('[^0-9]',"")
-    for i in df.index:
-        if len(df.iloc[i]['Num_pages']) >= 7 or len(df.iloc[i]['Num_pages']) <= 1:
-            df.at[i, 'Num_pages'] = np.NaN
-    df['Num_pages'] = df['Num_pages'].replace(r'^\s*$', np.NaN, regex=True)
-    df['Num_pages'] = pd.to_numeric(df['Num_pages'])
-    
-    # cleaning publish year
-    df.rename(columns={' Publishdate': 'Publishdate'}, inplace=True)
-    df['Publishdate'] = df.Publishdate.str.replace('[^0-9-]',"")
-    df['Publishdate'] = df['Publishdate'].replace(r'^\s*$', np.NaN, regex=True)
-    df['original_publish_year'] = df['Publishdate'].str[-4:]
-    df = df.drop(['Publishdate'], axis= 1)
-
-    # Calculating normalized ratings
-    df['minmax_norm_ratings'] = 1 + (df['avg_ratings'] - df.avg_ratings.min()) / (df.avg_ratings.max()-df.avg_ratings.min()) *9
-    df['mean_norm_ratings'] = 1 + ((df['avg_ratings'] - df.avg_ratings.mean()) / (df.avg_ratings.max()-df.avg_ratings.min())) *9
-
-    # delete last 7 rows    
-    df.drop(df.tail(7).index, inplace = True) 
-
-    # delete unnecessary columns
-    df.drop(['Unnamed: 0', 'id'], axis = 1, inplace = True)
-
-    return df
-
-preprocessing()
-
-df = preprocessing()
-
-
 # In[ ]:
+# Page settings
+st.set_page_config(
+	layout="centered",  # Can be "centered" or "wide". In the future also "dashboard", etc.
+	initial_sidebar_state="collapsed",  # Can be "auto", "expanded", "collapsed"
+	page_title=None,  # String or None. Strings get appended with "• Streamlit". 
+	page_icon=None,  # String, anything supported by st.image, or None.
+)
+
+import base64
+
+main_bg = "background.png"
+main_bg_ext = "png"
+
+#side_bg = "sample.jpg"
+#side_bg_ext = "jpg"
+
+st.markdown(
+    f"""
+    <style>
+    .reportview-container {{
+        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+    }} </style>""", unsafe_allow_html=True)
 
 
-# TITLE
-st.markdown("![The world of fantasy](https://media.giphy.com/media/3o7bu7BKhyOJt36jp6/source.gif)")
+audio_file = open("./MUSIC/background_music.mp3", "rb")
+audio_bytes = audio_file.read()
+st.audio(audio_bytes, format="audio/ogg")
 
-st.title("Welcome to the world of fantasy")
-
-
-# In[45]:
-
-
-# Show our data
-
-st.header("The data of the 1000 best epic fantasy books")
-
-st.markdown("""Take a look at the infos""")
-
-st.sidebar.header("Visualization Selector")
-#st.sidebar.text("Writing on the sidebar!")
-st.sidebar.markdown("""Tm!""")
-
-
-#df = pd.read_csv("example.csv")
-placeholder = st.empty()
-if st.sidebar.checkbox("First select to see the data"):
-    placeholder.dataframe(df)
+st.markdown("# Ready for the adventure ?")
+passw=st.radio("Enter the password",("Dont know","Alohomora","wingardium leviosa"))
+if passw == "Alohomora":
+    st.title("Welcome to the magical world of fantasy.")
+    st.markdown("![Welcome to Hogwarts](https://media.giphy.com/media/3oFzmbQom9DGp7OhZS/giphy.gif)")
+    st.header('We are team Hufflepuff')
+    st.markdown("![The world of fantasy](https://media.giphy.com/media/PMp40oEvNfKve/giphy.gif)")
     
-#if st.sidebar.button("Show me the data."):
-#    st.dataframe(df)
+    # TITLE
+        
+    # Show our data
 
-# Choose columns to show
-#st.sidebar.markdown("")
-columns_to_show = st.sidebar.multiselect('If you want to display only few columns, you can chose here:', df.columns)
+    st.header("Explore the data of the 1000 best epic fantasy books")
 
-# Books with rating higher than X
-#st.markdown("")
-s1_min = int(df.minmax_norm_ratings.min())
-s1_max = int(df.minmax_norm_ratings.max())
-threshold = st.sidebar.slider("Just show books with a rating higher than:", s1_min, s1_max)
-filtered = df[df["minmax_norm_ratings"] >= threshold]
-st.dataframe(filtered[columns_to_show])
+    st.markdown("""Epic fantasy is generally serious in tone and often epic in scope, dealing with themes of grand struggle against supernatural, evil forces. Some typical characteristics of epic fantasy include fantastical elements such as elves, fairies, dwarves, magic or sorcery, wizards or magicians, invented languages, quests, coming-of-age themes, and multi-volume narratives.""")
 
+    df = pd.read_csv("Data_graphs.csv")
+    if st.button("Show me the data."):
+        st.dataframe(df)
 
-# Show the 2 scatterplots of pages and num_ratings / num_awards
-st.subheader("What can we say about these nice scatterplots")
+    # Choose columns to show
+    st.markdown("If you want to display only few columns, you can chose here:")
+    columns_to_show = st.multiselect('', df.columns)
 
-df_s = df.dropna(subset=['Num_pages', 'number_of_ratings'])
-plt.figure(figsize=(20,10))
-x1  = df_s['Num_pages'].astype(int)
-x2 = df_s['Num_pages'].astype(int)
+    # Books with rating higher than X
+    st.markdown("Just show books with a rating higher than:")
+    s1_min = float(df.minmax_norm_ratings.min())
+    s1_max = float(df.minmax_norm_ratings.max())
+    threshold = st.slider("", s1_min, s1_max)
+    filtered = df[df["minmax_norm_ratings"] >= threshold]
+    st.dataframe(filtered[columns_to_show])
 
-y1 = df_s['number_of_ratings'].astype(int)
-y2 = df_s['award_count'].astype(int)
+    # Sidebar
+    st.sidebar.header("Who is your favourite fantasy author?")
+    #col1 = st.sidebar.beta_columns(1)
+    user_input = st.sidebar.text_input("", "Suzanne Collins")
+    dg = df.loc[df["Author"] == user_input].sort_values(by=['avg_ratings'], ascending= False).head(1)
+    
+    st.sidebar.write("Want to read her/his highest rated book?")
+    for i in range(1, 2):
+        cols = st.sidebar.beta_columns(1)
+        cols[0].table(dg['Setting']) #use_column_width=True)
+    for i in range(1, 2):
+        cols = st.sidebar.beta_columns(1)
+        cols[0].table(dg['Series']) #use_column_width=True)
 
-plt.subplot(1,2,1)
-plt.scatter(x1, y1, color='blue')
-plt.xlabel("Number of pages")
-plt.ylabel("Number of ratings")
+    isbn = 3789120022
 
-plt.subplot(1,2,2)
-plt.scatter(x2, y2, color='red')
-plt.xlabel("Number of pages")
-plt.ylabel("Number of awards")
-
-plt.tight_layout()
-st.pyplot(plt)
-
-
-# Show the number of awards with the amount of books in it
-dz = df['award_count'].value_counts()
-dw= dz.sort_index()
-dw = pd.DataFrame(dw)
-dw.reset_index(inplace=True)
-dw = dw.rename(columns = {'index':'award_count', 'award_count': 'num_books'})
-sns.set_context('talk')
-sns.set_style('whitegrid')
-plt.figure(figsize=(14, 8))
-ax= sns.barplot(y=dw['num_books'],x=dw["award_count"])
-ax.set_yscale("log")
-for p in ax.patches:
-    ax.annotate(format(int(p.get_height())), 
-                   (p.get_x() + p.get_width() / 2., p.get_height()), 
-                   ha = 'center', va = 'center', 
-                   xytext = (0, 9), 
-                   textcoords = 'offset points')
-ax= plt.xlabel('Number of awards')
-ax= plt.ylabel('Number of books')
-
-st.pyplot(plt)
-
-# Show a list of the TOP TEN awarded books 
-st.subheader("And the most awards for best EPIC fantasy book goes to...")
-st.markdown("![The best EPIC fantasy book](https://media.giphy.com/media/c35lTs1WBrqJW/source.gif)")
-df_sorted = df.sort_values(by='award_count', ascending=False).head(10)
-st.dataframe(df_sorted['Title'])
-
-# Books with more awards than X
-#st.markdown("")
-s2_min = int(df.award_count.min())
-s2_max = int(df.award_count.max())
-threshold = st.slider("Just show books with more awards than:", s2_min, s2_max)
-filtered = df[df["award_count"] >= threshold]
-if st.checkbox("Select by awards"):
-    placeholder.dataframe(filtered[columns_to_show])
-#st.dataframe(filtered[columns_to_show])
-
-# More Ratings = More Awards? 
-st.subheader("Here you can see the number of ratings with the number of awards")
-sns.set_context('talk')
-sns.set_style('whitegrid')
-sns.set_palette(['purple'])
-plt.figure(figsize=(20, 10))
-ax= sns.regplot(data=preprocessing(), y="number_of_ratings", x="award_count")
-ax= plt.ylabel("Number of awards")
-ax= plt.xlabel("Number of ratings")
-
-st.pyplot(plt)
+    link = f'[BUY YOUR FAVORITE BOOK FROM {user_input} ON AMAZON] (https://www.amazon.de/s?k={isbn}&__mk_de_DE=ÅMÅŽÕÑ&ref=nb_sb_noss)'
+    st.sidebar.markdown(link, unsafe_allow_html=True)        
+        #cols[1].write(f'{i * i}')
+        #cols[2].write(f'{i * i * i}')
+        #cols[3].write('x' * i)
+    
+    #for i in range(1, 2):
+    #    cols = st.sidebar.beta_columns(1)
+    #    cols[0].image(avg_rating_img, use_column_width=True)
+    #for i in range(1, 2):
+    #    cols = st.sidebar.beta_columns(1)
+    #    cols[0].image(mean_norm_rating_img, use_column_width=True)
+    #for i in range(1, 2):
+    #    cols = st.sidebar.beta_columns(1)
+    #    cols[0].image(all_fitted_dist_img, use_column_width=True)
+    #for i in range(1, 2):
+    #    cols = st.sidebar.beta_columns(1)
+    #    cols[0].image(avg_rat_burr_img, use_column_width=True)
 
 
-# In[ ]:
+    # Show the 2 scatterplots of pages and num_ratings / num_awards
+    st.markdown("""Got a long attention span? No? We thought so too, the data supports that too. You are all good :D!
+People love books that are not too long, it just keeps us interested. Facts! The graphs do not lie, that is why we award such books greatly too!""")
+
+    df_s = df.dropna(subset=['Num_pages', 'number_of_ratings'])
+    plt.figure(figsize=(20,10))
+    x1  = df_s['Num_pages'].astype(int)
+    x2 = df_s['Num_pages'].astype(int)
+
+    y1 = df_s['number_of_ratings'].astype(int)
+    y2 = df_s['award_count'].astype(int)
+
+    plt.subplot(1,2,1)
+    plt.scatter(x1, y1, color='blue')
+    plt.xlabel("Number of pages")
+    plt.ylabel("Number of ratings")
+
+    plt.subplot(1,2,2)
+    plt.scatter(x2, y2, color='red')
+    plt.xlabel("Number of pages")
+    plt.ylabel("Number of awards")
+
+    plt.tight_layout()
+    st.pyplot(plt)
 
 
+    avg_rating_img = Image.open("./IMG/average_rating.png")
+    mean_norm_rating_img = Image.open("./IMG/mean_normalized_rating.png")
+    all_fitted_dist_img = Image.open("./IMG/all_fitted_dist.png")
+    avg_rat_burr_img = Image.open("./IMG/avg_rat_burr.png")
+
+    st.header("You are a curious elf and want to dive even deeper?")
+    for i in range(1, 2):
+        cols = st.beta_columns(4)
+        cols[0].image(avg_rating_img, use_column_width=True)
+        cols[1].image(mean_norm_rating_img, use_column_width=True)
+        cols[2].image(all_fitted_dist_img, use_column_width=True)
+        cols[3].image(avg_rat_burr_img, use_column_width=True)
+
+    for i in range(1, 2):
+        cols = st.beta_columns(4)
+        cols[0].write('Histogram for average rating with supporting boxplot.', use_column_width=True)
+        cols[1].write("Boxplot and histogram for min-max normalized ratings.", use_column_width=True)
+        cols[2].write('Several possible fits were tested on the distribution graphs.', use_column_width=True)
+        cols[3].write('The burr distribution was the best fit.', use_column_width=True)
+
+    st.markdown("![The best EPIC fantasy book](https://media.giphy.com/media/Y1vq7S47uaH1hqYhf6/source.gif)")
+
+    # AWARDS
+
+    st.markdown('Like the protagonists in their stories, the books fight relentlessly for honor and profit')
+    # Show the number of awards with the amount of books in it
+    dz = df['award_count'].value_counts()
+    dw= dz.sort_index()
+    dw = pd.DataFrame(dw)
+    dw.reset_index(inplace=True)
+    dw = dw.rename(columns = {'index':'award_count', 'award_count': 'num_books'})
+    sns.set_context('talk')
+    sns.set_style('whitegrid')
+    plt.figure(figsize=(14, 8))
+    ax= sns.barplot(y=dw['num_books'],x=dw["award_count"])
+    ax.set_yscale("log")
+    for p in ax.patches:
+        ax.annotate(format(int(p.get_height())), 
+                    (p.get_x() + p.get_width() / 2., p.get_height()), 
+                    ha = 'center', va = 'center', 
+                    xytext = (0, 9), 
+                    textcoords = 'offset points')
+    ax= plt.xlabel('Number of awards')
+    ax= plt.ylabel('Number of books')
+
+    st.pyplot(plt)
+
+    # Show a list of the TOP TEN awarded books 
+    st.subheader("And the most awards for best EPIC fantasy book go to...")
+    st.markdown("![The best EPIC fantasy book](https://media.giphy.com/media/c35lTs1WBrqJW/source.gif)")
+    df_sorted = df.sort_values(by='award_count', ascending=False).head(10)
+    st.dataframe(df_sorted['Title'])
+
+    # Books with more awards than X
+    #st.markdown("")
+    s2_min = int(df.award_count.min())
+    s2_max = int(df.award_count.max())
+    threshold = st.slider("Just show books with more awards than:", s2_min, s2_max)
+    filtered = df[df["award_count"] >= threshold]
+    if st.checkbox("Select by awards"):
+        placeholder = st.empty()
+        placeholder.dataframe(filtered[columns_to_show])
+    #st.dataframe(filtered[columns_to_show])
+
+    # More Ratings = More Awards? 
+    st.subheader("Here you can see the number of ratings with the number of awards")
+    sns.set_context('talk')
+    sns.set_style('whitegrid')
+    sns.set_palette(['purple'])
+    plt.figure(figsize=(20, 10))
+    ax= sns.regplot(data=df, y="number_of_ratings", x="award_count")
+    ax= plt.ylabel("Number of awards")
+    ax= plt.xlabel("Number of ratings")
+
+    st.pyplot(plt)
 
 
-
-# In[ ]:
+else:
+    st.markdown("![You shall not pass](https://media.giphy.com/media/nlcJ4MkoG3Tri/giphy.gif)") 
+    st.title("You shall not pass")
 
 
 # Interactive Genre List
---
+++ visualization/app_4_back.py
@@ -0,0 +1,232 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats 
+from matplotlib.ticker import MaxNLocator
+
+import streamlit as st
+from PIL import Image # Required to show images
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+# In[41]:
+
+
+# just for production ... should be done in seperate Notebook
+# In[ ]:
+# Page settings
+st.set_page_config(
+	layout="centered",  # Can be "centered" or "wide". In the future also "dashboard", etc.
+	initial_sidebar_state="collapsed",  # Can be "auto", "expanded", "collapsed"
+	page_title=None,  # String or None. Strings get appended with "• Streamlit". 
+	page_icon=None,  # String, anything supported by st.image, or None.
+)
+
+import base64
+
+main_bg = "background.png"
+main_bg_ext = "png"
+
+#side_bg = "sample.jpg"
+#side_bg_ext = "jpg"
+
+st.markdown(
+    f"""
+    <style>
+    .reportview-container {{
+        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
+    }} </style>""", unsafe_allow_html=True)
+
+
+audio_file = open("./MUSIC/background_music.mp3", "rb")
+audio_bytes = audio_file.read()
+my_expander = st.beta_expander(' ')
+my_expander.audio(audio_bytes, format="audio/ogg")
+
+st.markdown("# Ready for the adventure ?")
+passw=st.radio("Enter the password",("Dont know","Alohomora","wingardium leviosa"))
+if passw == "Alohomora":
+    st.title("Welcome to the magical world of fantasy.")
+    st.markdown("![Welcome to Hogwarts](https://media.giphy.com/media/3oFzmbQom9DGp7OhZS/giphy.gif)")
+    st.header('We are team Hufflepuff')
+    st.markdown("![The world of fantasy](https://media.giphy.com/media/PMp40oEvNfKve/giphy.gif)")
+    
+    # TITLE
+        
+    # Show our data
+
+    st.header("Explore the data of the 1000 best epic fantasy books")
+
+    st.markdown("""Got a long attention span? No? We thought so too, the data supports that too. You are all good :D!
+People love books that are not too long, it just keeps us interested. Facts! The graphs do not lie, that is why we award such books greatly too!""")
+
+    df = pd.read_csv("Data_graphs.csv")
+    if st.button("Show me the data."):
+        st.dataframe(df)
+
+    # Choose columns to show
+    st.markdown("If you want to display only few columns, you can chose here:")
+    columns_to_show = st.multiselect('', df.columns)
+
+    # Books with rating higher than X
+    st.markdown("Just show books with a rating higher than:")
+    s1_min = float(df.minmax_norm_ratings.min())
+    s1_max = float(df.minmax_norm_ratings.max())
+    threshold = st.slider("", s1_min, s1_max)
+    filtered = df[df["minmax_norm_ratings"] >= threshold]
+    st.dataframe(filtered[columns_to_show])
+
+    # Sidebar
+    st.sidebar.header("Who is your favourite fantasy author?")
+    #col1 = st.sidebar.beta_columns(1)
+    user_input = st.sidebar.text_input("", "Suzanne Collins")
+    dg = df.loc[df["Author"] == user_input].sort_values(by=['avg_ratings'], ascending= False).head(1)
+    
+    st.sidebar.write("Want to read her/his highest rated book?")
+    for i in range(1, 2):
+        cols = st.sidebar.beta_columns(1)
+        cols[0].table(dg['Setting']) #use_column_width=True)
+    for i in range(1, 2):
+        cols = st.sidebar.beta_columns(1)
+        cols[0].table(dg['Series']) #use_column_width=True)
+
+    isbn = 3789120022
+
+    link = f'[BUY YOUR FAVORITE BOOK FROM {user_input} ON AMAZON] (https://www.amazon.de/s?k={isbn}&__mk_de_DE=ÅMÅŽÕÑ&ref=nb_sb_noss)'
+    st.sidebar.markdown(link, unsafe_allow_html=True)        
+        #cols[1].write(f'{i * i}')
+        #cols[2].write(f'{i * i * i}')
+        #cols[3].write('x' * i)
+    
+    #for i in range(1, 2):
+    #    cols = st.sidebar.beta_columns(1)
+    #    cols[0].image(avg_rating_img, use_column_width=True)
+    #for i in range(1, 2):
+    #    cols = st.sidebar.beta_columns(1)
+    #    cols[0].image(mean_norm_rating_img, use_column_width=True)
+    #for i in range(1, 2):
+    #    cols = st.sidebar.beta_columns(1)
+    #    cols[0].image(all_fitted_dist_img, use_column_width=True)
+    #for i in range(1, 2):
+    #    cols = st.sidebar.beta_columns(1)
+    #    cols[0].image(avg_rat_burr_img, use_column_width=True)
+
+
+    # Show the 2 scatterplots of pages and num_ratings / num_awards
+    st.subheader("""Books that have less than 1500 pages have higher number of ratings this may be due to the fact that they capture and keep the readers interested. We theorize this to be down to the human attention span being short.
+It is interesting but not surprising to see that the books with less than 1500 pages also have more awards, once again pointing to the fact that this might be down to the fact that the page range is optimal to capture reader's interest and sustain the interest.""")
+
+    df_s = df.dropna(subset=['Num_pages', 'number_of_ratings'])
+    plt.figure(figsize=(20,10))
+    x1  = df_s['Num_pages'].astype(int)
+    x2 = df_s['Num_pages'].astype(int)
+
+    y1 = df_s['number_of_ratings'].astype(int)
+    y2 = df_s['award_count'].astype(int)
+
+    plt.subplot(1,2,1)
+    plt.scatter(x1, y1, color='blue')
+    plt.xlabel("Number of pages")
+    plt.ylabel("Number of ratings")
+
+    plt.subplot(1,2,2)
+    plt.scatter(x2, y2, color='red')
+    plt.xlabel("Number of pages")
+    plt.ylabel("Number of awards")
+
+    plt.tight_layout()
+    st.pyplot(plt)
+
+
+    avg_rating_img = Image.open("./IMG/average_rating.png")
+    mean_norm_rating_img = Image.open("./IMG/mean_normalized_rating.png")
+    all_fitted_dist_img = Image.open("./IMG/all_fitted_dist.png")
+    avg_rat_burr_img = Image.open("./IMG/avg_rat_burr.png")
+
+    st.subheader("You are a curious elf and want to dive even deeper?")
+    for i in range(1, 2):
+        cols = st.beta_columns(4)
+        cols[0].image(avg_rating_img, use_column_width=True)
+        cols[1].image(mean_norm_rating_img, use_column_width=True)
+        cols[2].image(all_fitted_dist_img, use_column_width=True)
+        cols[3].image(avg_rat_burr_img, use_column_width=True)
+
+    for i in range(1, 2):
+        cols = st.beta_columns(4)
+        cols[0].write('Histogram for average rating with supporting boxplot.', use_column_width=True)
+        cols[1].image(mean_norm_rating_img, use_column_width=True)
+        cols[2].image(all_fitted_dist_img, use_column_width=True)
+        cols[3].image(avg_rat_burr_img, use_column_width=True)
+
+    # AWARDS
+
+    st.markdown('Like the protagonists in their stories, the books fight relentlessly for honor and profit')
+    # Show the number of awards with the amount of books in it
+    dz = df['award_count'].value_counts()
+    dw= dz.sort_index()
+    dw = pd.DataFrame(dw)
+    dw.reset_index(inplace=True)
+    dw = dw.rename(columns = {'index':'award_count', 'award_count': 'num_books'})
+    sns.set_context('talk')
+    sns.set_style('whitegrid')
+    plt.figure(figsize=(14, 8))
+    ax= sns.barplot(y=dw['num_books'],x=dw["award_count"])
+    ax.set_yscale("log")
+    for p in ax.patches:
+        ax.annotate(format(int(p.get_height())), 
+                    (p.get_x() + p.get_width() / 2., p.get_height()), 
+                    ha = 'center', va = 'center', 
+                    xytext = (0, 9), 
+                    textcoords = 'offset points')
+    ax= plt.xlabel('Number of awards')
+    ax= plt.ylabel('Number of books')
+
+    st.pyplot(plt)
+
+    # Show a list of the TOP TEN awarded books 
+    st.subheader("And the most awards for best EPIC fantasy book go to...")
+    st.markdown("![The best EPIC fantasy book](https://media.giphy.com/media/c35lTs1WBrqJW/source.gif)")
+    df_sorted = df.sort_values(by='award_count', ascending=False).head(10)
+    st.dataframe(df_sorted['Title'])
+
+    # Books with more awards than X
+    #st.markdown("")
+    s2_min = int(df.award_count.min())
+    s2_max = int(df.award_count.max())
+    threshold = st.slider("Just show books with more awards than:", s2_min, s2_max)
+    filtered = df[df["award_count"] >= threshold]
+    if st.checkbox("Select by awards"):
+        placeholder = st.empty()
+        placeholder.dataframe(filtered[columns_to_show])
+    #st.dataframe(filtered[columns_to_show])
+
+    # More Ratings = More Awards? 
+    st.subheader("Here you can see the number of ratings with the number of awards")
+    sns.set_context('talk')
+    sns.set_style('whitegrid')
+    sns.set_palette(['purple'])
+    plt.figure(figsize=(20, 10))
+    ax= sns.regplot(data=df, y="number_of_ratings", x="award_count")
+    ax= plt.ylabel("Number of awards")
+    ax= plt.xlabel("Number of ratings")
+
+    st.pyplot(plt)
+
+
+else:
+    st.markdown("![You shall not pass](https://media.giphy.com/media/nlcJ4MkoG3Tri/giphy.gif)") 
+    st.title("You shall not pass")
+
+
+# Interactive Genre List
+
+# 8 Visualize the awards distribution in a barplot with a list of books shown when clicked on a bar
+
